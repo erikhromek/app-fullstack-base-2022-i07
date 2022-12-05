b330e6f7d9128edd4ac923c60ba3f39ca99f26c0
Erik Hromek <erikhromek96@gmail.com>
2022-12-05 02:11:18 -0300

    Add device modal

--- src/frontend/ts/device.ts
+++ src/frontend/ts/device.ts
@@ -1,7 +1,24 @@
+enum Type {
+  Switch = 0,
+  Stepper = 1,
+  Dimmer = 2
+}
 class Device{
   public id: number;
   public name: string;
-  public state: boolean;
   public description: string;
-  public type: number;
+  public type: Type;
+  public state: number;
+
+  constructor(name: string, description: string, type: Type) {
+    this.name = name;
+    this.description = description;
+    this.type = type;
+    this.state = 0;
+
+}
+
+public toString(): string{
+    return `Dispositivo: ${this.name} (${this.type}) - Estado:  ${this.state}`;
+}
 }
